a88199d27b4d0b6fb090541c615a2fb6967973e3
nonnpix-jpg <nastaaprokofevaa@gmail.com>
2026-09-24 11:14:41 +0300

    Добавлен класс отражающий объект таблицы products_is_in_stock

---
+++ InventoryManagementSystem/ClassLibrary/ProductIsInStock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class ProductIsInStock
+    {
+        public Product Product { get; set; }
+        public double QuantityInStock { get; set; }
+        public double QuantityForSale { get; set; }
+    }
+}
