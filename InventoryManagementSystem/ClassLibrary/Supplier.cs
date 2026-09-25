95ea3cd12deb132e9bc1e78976fc467c8a0bf8fb
GavrishM <nanotechnology930@gmail.com>
2026-09-25 05:06:57 +0300

    Улучшен класс Supplier и таблица suppliers

--- InventoryManagementSystem/ClassLibrary/Supplier.cs
+++ InventoryManagementSystem/ClassLibrary/Supplier.cs
@@ -10,11 +10,20 @@ namespace ClassLibrary
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
 
         public Supplier(int id, string name)
         {
             Id = id;
             Name = name;
+        }
+        public Supplier(int id, string name, string phone, string email)
+        {
+            Id = id;
+            Name = name;
+            Phone = phone;
+            Email = email;
         }
     }
 }
