a713f493c9fa95f653bb7786269d4157551eb2e3
GavrishM <nanotechnology930@gmail.com>
2026-09-25 05:29:06 +0300

    Добавлен класс Log

---
+++ InventoryManagementSystem/ClassLibrary/Log.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Log
+    {
+        public int Id { get; set; }
+        public Product Product { get; set; }
+        public string Type { get; set; }
+        public double Quantity { get; set; }
+
+        public Log(int id, Product product)
+        {
+            Id = id;
+            Product = product;
+        }
+        public Log(int id, Product product, string type, double quantity)
+        {
+            Id = id;
+            Product = product;
+            Type = type;
+            Quantity = quantity;
+        }
+    }
+}
