2f20f8180c1c24b2a5f8246ec0b3ea021d75cedc
nonnpix-jpg <nastaaprokofevaa@gmail.com>
2026-09-25 09:25:59 +0300

    Id и Role в User Убраны

--- InventoryManagementSystem/ClassLibrary/User.cs
+++ InventoryManagementSystem/ClassLibrary/User.cs
@@ -8,39 +8,29 @@ namespace ClassLibrary
 {
     public class User
     {
-        public int Id { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
-        public string Role { get; set; }
 
-        public User (int id,
-                     string login,
-                     string password,
-                     string role)
+        public User (string login,
+                     string password)
         {
-            Id = id;
             Login = login;
             Password = password;
-            Role = role;
         }
-        public User(int id,
-                    string login,
+        public User(string login,
                     string password,
                     string name,
                     string email,
-                    string phone,
-                    string role)
+                    string phone)
         {
-            Id = id;
             Login = login;
             Password = password;
             Name = name;
             Email = email;
             Phone = phone;
-            Role = role;
         }
     }
 }
