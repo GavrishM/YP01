6adaac503e1386eab72bca5bfabd2f4014ca431b
GavrishM <nanotechnology930@gmail.com>
2026-09-25 05:57:50 +0300

    ProductsInStock добавлены конструкторы

--- InventoryManagementSystem/ClassLibrary/ProductIsInStock.cs
+++ InventoryManagementSystem/ClassLibrary/ProductIsInStock.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,22 @@ namespace ClassLibrary
 {
     public class ProductIsInStock
     {
+        public int Id { get; set; }
         public Product Product { get; set; }
         public double QuantityInStock { get; set; }
         public double QuantityForSale { get; set; }
+
+        public ProductIsInStock(int id, Product product)
+        {
+            Id = id;
+            Product = product;
+        }
+        public ProductIsInStock(int id, Product product, double quantityInStock, double quantityForSale)
+        {
+            Id = id;
+            Product = product;
+            QuantityInStock = quantityInStock;
+            QuantityForSale = quantityForSale;
+        }
     }
 }
